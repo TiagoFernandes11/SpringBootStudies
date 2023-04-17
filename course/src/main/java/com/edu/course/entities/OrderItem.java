75325560254796a62e0582eebe6520c95b0bd6cd
Tiago Fernandes Ribeiro <tiagofernandesribeiro@yahoo.com.br>
2023-04-17 17:14:08 -0300

    Subtotal & Total methods

--- course/src/main/java/com/edu/course/entities/OrderItem.java
+++ course/src/main/java/com/edu/course/entities/OrderItem.java
@@ -39,7 +39,7 @@ public class OrderItem implements Serializable{
         id.setOrder(order);
     }
 
-    public Product geProduct() {
+    public Product getProduct() {
         return id.getProduct();
     }
 
@@ -61,6 +61,10 @@ public class OrderItem implements Serializable{
 
     public void setPrice(Double price) {
         this.price = price;
+    }
+
+    public Double getSubTotal(){
+        return (this.price * this.quantity);
     }
 
     @Override
